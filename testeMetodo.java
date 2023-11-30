c8181ac85e04ceedf9158c5249c046341e999086
Clara Santos <147684476+maaclara@users.noreply.github.com>
2023-11-30 06:57:43 -0300

    Create testeMetodo.java

---
+++ testeMetodo.java
@@ -0,0 +1,15 @@
+package com.alura;
+
+public class TesteMetodo {
+	public static void main(String[] args) {
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
+		
+		boolean conseguiuRetirar = contaDoPaulo.saca(20);
+        System.out.println(contaDoPaulo.saldo);
+        System.out.println(conseguiuRetirar);
+	}
+
+}
