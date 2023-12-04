fd3eaaaf6d387e906cd6d3edd78caab558b4564b
Clara Santos <147684476+maaclara@users.noreply.github.com>
2023-12-04 06:23:51 -0300

    Update testeMetodo.java

--- testeMetodo.java
+++ testeMetodo.java
@@ -8,8 +8,19 @@ public class TesteMetodo {
 		System.out.println(contaDoPaulo.saldo);
 		
 		boolean conseguiuRetirar = contaDoPaulo.saca(20);
-        	System.out.println(contaDoPaulo.saldo);
-        	System.out.println(conseguiuRetirar);
+        System.out.println(contaDoPaulo.saldo);
+        System.out.println(conseguiuRetirar);
+       
+        Conta contaDaMarcela = new Conta();
+        contaDaMarcela.deposita(1000);
+        
+        boolean sucessoTransferencia = contaDaMarcela.transfere(300, contaDoPaulo);
+        if(sucessoTransferencia) {
+            System.out.println("transferencia com sucesso");
+        } else {
+            System.out.println("faltou dinheiro");
+        }
+        System.out.println(contaDaMarcela.saldo);
+        System.out.println(contaDoPaulo.saldo); 
 	}
-
 }
