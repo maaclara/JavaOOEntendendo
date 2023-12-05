349a22a9930f5aa51fe72c1a9250a7b6435b3051
Clara Santos <147684476+maaclara@users.noreply.github.com>
2023-12-05 06:23:57 -0300

    Update TestaBanco.java

--- bytebank-composto/TestaBanco.java
+++ bytebank-composto/TestaBanco.java
@@ -10,7 +10,9 @@ public class TestaBanco {
 		Conta contaDoPaulo = new Conta();
 		contaDoPaulo.deposita(100);
 		
+		// associa o cliente paulo a conta contaDoPaulo
 		contaDoPaulo.titular = paulo;
+		
 		System.out.println(contaDoPaulo.titular.nome);
 		 System.out.println(contaDoPaulo.titular);
 		 System.out.println(paulo);
