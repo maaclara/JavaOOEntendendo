7de2670e498dbc82c4624c801604bb83568d18f7
Clara Santos <147684476+maaclara@users.noreply.github.com>
2023-12-08 07:07:41 -0300

    Update Conta.java

--- bytebank-encapsulado/Conta.java
+++ bytebank-encapsulado/Conta.java
@@ -51,5 +51,13 @@ public class Conta {
     public void setAgencia(int agencia) {
 		this.agencia = agencia;
 	}
+    
+    public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+    
+    public Cliente getTitular() {
+        return titular;
+    }
   
 }
