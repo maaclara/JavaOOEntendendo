622c2f0e72f94d92ef5b0c3f22c33faeb85965f5
Clara Santos <147684476+maaclara@users.noreply.github.com>
2023-12-08 07:08:04 -0300

    Update TesteGetESet.java

--- bytebank-encapsulado/TesteGetESet.java
+++ bytebank-encapsulado/TesteGetESet.java
@@ -6,6 +6,27 @@ public class TestaGetESet {
 		//conta.numero = 1337;
 		conta.setNumero(1337);
 		System.out.println(conta.getNumero());
+		
+		Cliente paulo = new Cliente();
+		//conta.titular = paulo;
+		paulo.setNome("paulo silveira");
+
+        conta.setTitular(paulo);
+
+        System.out.println(conta.getTitular().getNome());
+        
+        conta.getTitular().setProfissao("programador"); 
+        //agora em duas linhas: 
+        Cliente titularDaConta = conta.getTitular();
+        titularDaConta.setProfissao("programador");
+		 
+        
+        System.out.println(titularDaConta); 
+        System.out.println(paulo);
+        System.out.println(conta.getTitular());
+        
+        //Ao executarmos a aplicação, veremos que todas as referências,
+        // são para o mesmo endereço de memória Cliente@...
 	}
 
 }
