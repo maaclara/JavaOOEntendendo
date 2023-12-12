adccddd19717620b52487e9202d6129676901fe3
Clara Santos <147684476+maaclara@users.noreply.github.com>
2023-12-12 06:58:24 -0300

    Create TestaValores.java

---
+++ bytebank-encapsulado/TestaValores.java
@@ -0,0 +1,18 @@
+package bytebank_encapsulado;
+
+public class TestaValores {
+	public static void main(String[] args) {
+		Conta conta = new Conta(1337, 24226);
+		
+	
+        System.out.println(conta.getAgencia());
+        
+        
+        Conta conta2 = new Conta(1337, 16549);
+        Conta conta3 = new Conta(1337, 14660);
+        
+        System.out.println(Conta.getTotal());
+        
+	}
+
+}
