7c59faaaaf8c022cd01274e57c473c3229c50112
Clara Santos <147684476+maaclara@users.noreply.github.com>
2023-12-12 06:57:37 -0300

    Update TesteGetESet.java

--- bytebank-encapsulado/TesteGetESet.java
+++ bytebank-encapsulado/TesteGetESet.java
@@ -2,9 +2,8 @@ package bytebank_encapsulado;
 
 public class TestaGetESet {
 	public static void main(String[] args) {
-		Conta conta = new Conta();
-		//conta.numero = 1337;
-		conta.setNumero(1337);
+		Conta conta = new Conta(1337, 24226);
+		
 		System.out.println(conta.getNumero());
 		
 		Cliente paulo = new Cliente();
